48fa79901bd418d670a13a04962d045e2dd6aaf4
Erik Olsson <eolsson@gmail.com>
2015-12-29 17:33:54 +0100

    Revert "Changed signature"
    This reverts commit 3d02243725a21f1b5d267d6c8ce6b6ca4d0edd50.

--- src/main/java/boot/service/TVService.java
+++ src/main/java/boot/service/TVService.java
@@ -1,13 +1,8 @@
 package boot.service;
 
 import boot.dto.ScheduleDTO;
-import boot.exception.NotFoundException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -16,43 +11,16 @@ import java.util.List;
 @Service
 public class TVService {
 
-    private AsyncRestTemplate restTemplate = new AsyncRestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
-    public DeferredResult<ScheduleDTO> nextOnAir(String showTitle) {
-
-        final DeferredResult<ScheduleDTO> result = new DeferredResult<>();
-
-        final DeferredResult<List<ScheduleDTO>> svt1 = getScheduledPrograms("svt1");
-
-        svt1.setResultHandler(svt1Result -> {
-
-            ((List<ScheduleDTO>) svt1Result).stream()
-                    .filter(program -> program.title.equalsIgnoreCase(showTitle))
-                    .findFirst()
-                    .ifPresent(result::setResult);
-
-            if (!result.hasResult()) {
-                result.setErrorResult(new NotFoundException(String.format("The program %s was not found", showTitle)));
-            }
-        });
-
-        return result;
+    public ScheduleDTO nextOnAir(String showTitle) {
+        // add logic to determine if the given show is on TV today
+        return null;
     }
 
-    public DeferredResult<List<ScheduleDTO>> getScheduledPrograms(String channel) {
-
-        DeferredResult<List<ScheduleDTO>> deferredResult = new DeferredResult<>();
-
-        final ListenableFuture<ResponseEntity<ScheduleDTO[]>> listenableFuture =
-                restTemplate.getForEntity("http://www.svt.se/play4api/channel/{channel}/schedule",
-                ScheduleDTO[].class, channel);
-
-        listenableFuture.addCallback(result -> {
-            deferredResult.setResult(Arrays.asList(result.getBody()));
-        }, deferredResult::setErrorResult);
-
-        return deferredResult;
-
+    public List<ScheduleDTO> getScheduledPrograms(String channel) {
+        return Arrays.asList(restTemplate.getForObject("http://www.svt.se/play4api/channel/{channel}/schedule",
+                ScheduleDTO[].class, channel));
     }
 
 }
